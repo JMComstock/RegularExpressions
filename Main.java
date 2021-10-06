f169e67bb857a05464345a1ac5f8d120c4e05097
Jason Comstock <71102533+JMComstock@users.noreply.github.com>
2021-10-06 13:49:04 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,138 @@
+package com.company;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String string = "I am a string. Yes I am.";
+        System.out.println(string);
+        String yourString = string.replaceAll("I", "You");
+        System.out.println(yourString);
+
+        String alphanumeric = "abcDeeeF12Ghhiiiijkl99z";
+        System.out.println(alphanumeric.replaceAll(".", "Y"));
+
+        System.out.println(alphanumeric.replaceAll("^abcDeee", "YYY"));
+
+        String secondString = "abcDeeeF12GhhabcDeeeiiiijkl99z";
+        System.out.println(secondString.replaceAll("^abcDeee", "YYY"));
+
+        System.out.println(alphanumeric.matches("^hello"));
+        System.out.println(alphanumeric.matches("^abcDeee"));
+        System.out.println(alphanumeric.matches("^abcDeeeF12Ghhiiiijkl99z"));
+
+        System.out.println(alphanumeric.replaceAll("ijkl99z$", "THE END"));
+        System.out.println(alphanumeric.replaceAll("[aei]", "X"));
+        System.out.println(alphanumeric.replaceAll("[aei]", "letter replaced"));
+        System.out.println(alphanumeric.replaceAll("[aei][Fj]", "X"));
+
+        System.out.println("harry".replaceAll("[Hh]arry", "Harry"));
+
+        String newAlphanumeric = "abcDeeeF12Ghhiiiijkl99z";
+        System.out.println(newAlphanumeric.replaceAll("[^ej]", "X"));
+        System.out.println(newAlphanumeric.replaceAll("[abcdef345678]", "X"));
+        System.out.println(newAlphanumeric.replaceAll("[a-fA-F3-8]", "X"));
+        System.out.println(newAlphanumeric.replaceAll("(?i)[a-f3-8]", "X")); // good for ascii streams, not unicode
+        System.out.println(newAlphanumeric.replaceAll("[0-9]", "X"));
+        System.out.println(newAlphanumeric.replaceAll("\\d", "X")); // replace all digits
+        System.out.println(newAlphanumeric.replaceAll("\\D", "X")); // replace all non-digits
+
+        String hasWhitespace = "I have blanks and\ta tab, and also a newline\n";
+        System.out.println(hasWhitespace);
+        System.out.println(hasWhitespace.replaceAll("\\s", "")); // removes all whitespace
+        System.out.println(hasWhitespace.replaceAll("\t", "X"));
+        System.out.println(hasWhitespace.replaceAll("\\S", "")); // replaces all non-whitespace characters, only tab, spaces and newline characters remain
+        System.out.println(newAlphanumeric.replaceAll("\\w", "X"));
+        System.out.println(hasWhitespace.replaceAll("\\w", "X"));
+        System.out.println(hasWhitespace.replaceAll("\\b", "X")); // places the replacement in front and behind of all the words in the string
+
+        String thirdAlphanumericString = "abcDeeeF12Ghhiiiijkl99z";
+        System.out.println(thirdAlphanumericString.replaceAll("^abcDe{3}", "YYY"));
+        System.out.println(thirdAlphanumericString.replaceAll("^abcDe+", "YYY"));
+        System.out.println(thirdAlphanumericString.replaceAll("^abcDe*", "YYY"));
+        System.out.println(thirdAlphanumericString.replaceAll("abcDe{2,5}", "YYY"));
+        System.out.println(thirdAlphanumericString.replaceAll("h+i*j", "Y"));
+
+        StringBuilder htmlText = new StringBuilder("<h1>My Heading</h1>");
+        htmlText.append("<h2>Sub-heading</h2>");
+        htmlText.append("<p>This is a paragraph about something.</p>");
+        htmlText.append("<p>This is a paragraph about something else.</p>");
+        htmlText.append("<h2>Summary</h2>");
+        htmlText.append("<p>Here is the summary.</p>");
+
+//        String h2Pattern = ".*<h2>.*"; // "." character class matches every character and the "*" quantifier means 0 or more
+        String h2Pattern = "<h2>";
+        Pattern pattern = Pattern.compile(h2Pattern);
+        Matcher matcher = pattern.matcher(htmlText);
+        System.out.println(matcher.matches());
+
+        matcher.reset();
+        int count = 0;
+        while(matcher.find()) {
+            count++;
+            System.out.println("Occurrence " + count + " : " + matcher.start() + " to " + matcher.end());
+
+        }
+
+        String h2GroupPattern = "(<h2>.*?</h2>)";
+        Pattern groupPattern = Pattern.compile(h2GroupPattern);
+        Matcher groupMatcher = groupPattern.matcher(htmlText);
+        System.out.println(groupMatcher.matches());
+        groupMatcher.reset();
+
+        while(groupMatcher.find()) {
+            System.out.println("Occurrence: " + groupMatcher.group(1));
+        }
+
+        String h2TextGroups = "(<h2>)(.+?)(</h2>)";
+        Pattern h2TextPattern = Pattern.compile(h2TextGroups);
+        Matcher h2TextMatcher = h2TextPattern.matcher(htmlText);
+
+        while(h2TextMatcher.find()) {
+            System.out.println("Occurrence: " + h2TextMatcher.group(2));
+        }
+
+        // "abc" means "a" and "b" and "c"
+        // [Hh]arry
+        System.out.println("harry".replaceAll("[H|h]arry", "Larry"));
+        System.out.println("Harry".replaceAll("[H|h]arry", "Larry"));
+
+        // [^abc] carrot character means not when it's the first expression
+        String tvTest = "tstvtkt";
+//        String tNotVRegExp = "t[^v]";
+        String tNotVRegExp = "t(?!v)"; // t which isn't followed by v
+        Pattern tNotVPattern = Pattern.compile(tNotVRegExp);
+        Matcher tNotVMatcher = tNotVPattern.matcher(tvTest);
+
+        count = 0;
+        while(tNotVMatcher.find()) {
+            count++;
+            System.out.println("Occurrence " + count + " : " + tNotVMatcher.start() + " to " + tNotVMatcher.end());
+        }
+
+        // t(?=v) find all matches with t followed by v but not include v in the match
+        // ^([\(]{1}[0-9]{3}[\)]{1}[ ]{1}[0-9]{3}[\-]{1}[0-9]{4})$     <--one way to verify a US phone number with regular expression
+        String phone1 = "1234567890"; // should not match
+        String phone2 = "(123) 456-7890"; // should match
+        String phone3 = "123 456-7890"; // shouldn't match
+        String phone4 = "(123)456-7890"; // shouldn't match
+
+        System.out.println("phone1 = " + phone1.matches("^([\\(]{1}[0-9]{3}[\\)]{1}[ ]{1}[0-9]{3}[\\-]{1}[0-9]{4})$"));
+        System.out.println("phone2 = " + phone2.matches("^([\\(]{1}[0-9]{3}[\\)]{1}[ ]{1}[0-9]{3}[\\-]{1}[0-9]{4})$"));
+        System.out.println("phone3 = " + phone3.matches("^([\\(]{1}[0-9]{3}[\\)]{1}[ ]{1}[0-9]{3}[\\-]{1}[0-9]{4})$"));
+        System.out.println("phone4 = " + phone4.matches("^([\\(]{1}[0-9]{3}[\\)]{1}[ ]{1}[0-9]{3}[\\-]{1}[0-9]{4})$"));
+
+        // ^4[0-9]{12}([0-9]{3})?$
+        String card1 ="4444444444444"; // should match
+        String card2 ="5444444444444";  // shouldn't match
+        String card3 ="4444444444444444"; // should match
+        String card4 ="4444"; // shouldn't match
+
+        System.out.println("card1 " + card1.matches("^4[0-9]{12}([0-9]{3})?$"));
+        System.out.println("card2 " + card2.matches("^4[0-9]{12}([0-9]{3})?$"));
+        System.out.println("card3 " + card3.matches("^4[0-9]{12}([0-9]{3})?$"));
+        System.out.println("card4 " + card4.matches("^4[0-9]{12}([0-9]{3})?$"));
+    }
+}
